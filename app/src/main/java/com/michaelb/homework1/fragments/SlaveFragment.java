d5ba9b9bfdde515d3fb01cf5a9a783eec39eee1c
Michael Berezovskyy <michael.berezovskyy@masterofcode.com>
2014-10-20 00:47:41 +0300

    Added two fragments but no communication between them

---
+++ app/src/main/java/com/michaelb/homework1/fragments/SlaveFragment.java
@@ -0,0 +1,23 @@
+package com.michaelb.homework1.fragments;
+
+/**
+ * Created by michaelb on 10/20/14.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+import com.michaelb.homework1.R;
+
+public class SlaveFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.slave_fragment, container, false);
+    }
+
+}
